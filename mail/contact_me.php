72139f296d2fbce01e323417ddf99a1f6eff1444
eNichts <eNichts@pinin.local>
2018-01-25 14:47:30 +0100

    Sendgrid test fix 2

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,17 +1,6 @@
 <?php
-require './vendor/autoload.php';
-require("./vendor/sendgrid/sendgrid/sendgrid-php.php");
-
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
+require '../vendor/autoload.php';
+require("../vendor/sendgrid/sendgrid/sendgrid-php.php");
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
