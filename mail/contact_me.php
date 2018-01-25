098650a6498b5d98a14e9ecee97645cfd6d1d3d6
eNichts <eNichts@pinin.local>
2018-01-25 15:06:13 +0100

    Sendgrid test fix

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,15 +1,19 @@
 <?php
-require '../vendor/autoload.php';
+// If you are using Composer
+require './vendor/autoload.php';
+
+// If you are not using Composer (recommended)
+// require("path/to/sendgrid-php/sendgrid-php.php");
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$from = new SendGrid\Email(null, "donotreply@ajax-notes.com");
-$subject = "Website contact from a user";
-$to = new SendGrid\Email(null, "lorydks891@gmail.com");
-$content = new SendGrid\Content("text/plain", "Test");
+$from = new SendGrid\Email(null, "test@example.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "test@example.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
@@ -18,7 +22,7 @@ $sg = new \SendGrid($apiKey);
 $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 echo $response->headers();
-echo $response->body(); 
+echo $response->body();
 
 return true;
 ?>
