db548067717beab0fca71c5f62a08468f2316b2c
eNichts <eNichts@pinin.local>
2018-01-25 14:55:06 +0100

    Another sendgrid test fix

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,5 @@
 <?php
 require '../vendor/autoload.php';
-require("../vendor/sendgrid/sendgrid/sendgrid-php.php");
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
@@ -10,7 +9,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $from = new SendGrid\Email(null, "donotreply@ajax-notes.com");
 $subject = "Website contact from a user";
 $to = new SendGrid\Email(null, "lorydks891@gmail.com");
-$content = new SendGrid\Content("text/plain", "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message");
+$content = new SendGrid\Content("text/plain", "Test");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
